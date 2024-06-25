034cedd0bbdf367fe70dd681a7a5209a4bd76e34
sppooo <166227545+sppooo@users.noreply.github.com>
2024-06-25 21:55:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React,{ useEffect } from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Spotify from './components/Spotify';
+import TrackInfoPage from "./components/TrackInfoPage";
 import { reducerCases } from "./utils/Constants";
 import { useStateProvider } from "./utils/StateProvider";
 
-
 export default function App() {
-   const [{token }, dispatch] = useStateProvider();
-   useEffect(()=> {
+   const [{ token }, dispatch] = useStateProvider();
+   useEffect(() => {
      const hash = window.location.hash;
      if (hash) {
-      const token = hash.substring(1).split("&")[0].split("=")[1];
-      dispatch({ type: reducerCases.SET_TOKEN, token });
+       const token = hash.substring(1).split("&")[0].split("=")[1];
+       dispatch({ type: reducerCases.SET_TOKEN, token });
      }
    }, [token, dispatch]);
- return <div>{token ? <Spotify /> : <Login />}</div>;
+
+   return (
+     <Router>
+       <Routes>
+         <Route path="/" element={token ? <Spotify /> : <Login />} />
+         <Route path="/track-info" element={<TrackInfoPage />} />
+       </Routes>
+     </Router>
+   );
 }
